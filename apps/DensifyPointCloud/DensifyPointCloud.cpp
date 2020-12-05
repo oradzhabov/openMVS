a7e1463f4643050be52de8d455e536ca4b2045dd
Piero Toffanin <pt@masseranolabs.com>
2020-12-05 15:13:40 -0500

    Remove file logging

--- apps/DensifyPointCloud/DensifyPointCloud.cpp
+++ apps/DensifyPointCloud/DensifyPointCloud.cpp
@@ -143,7 +143,7 @@ bool Initialize(size_t argc, LPCTSTR* argv)
 	}
 
 	// initialize the log file
-	OPEN_LOGFILE(MAKE_PATH(APPNAME _T("-")+Util::getUniqueName(0)+_T(".log")).c_str());
+	//OPEN_LOGFILE(MAKE_PATH(APPNAME _T("-")+Util::getUniqueName(0)+_T(".log")).c_str());
 
 	// print application details: version and command line
 	Util::LogBuild();
