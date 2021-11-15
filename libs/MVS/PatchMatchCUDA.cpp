4ba546384a8ac7b587617c09d4020527a345ace7
Piero Toffanin <pt@masseranolabs.com>
2021-11-15 11:24:59 -0500

    Double fNCCThresholdKeep

--- libs/MVS/PatchMatchCUDA.cpp
+++ libs/MVS/PatchMatchCUDA.cpp
@@ -276,7 +276,7 @@ void PatchMatchCUDA::EstimateDepthMap(DepthData& depthData)
 
 	// load depth-map, normal-map and confidence-map from CUDA memory
 	const float fNCCThresholdKeep(!params.bGeomConsistency && OPTDENSE::nEstimationGeometricIters ?
-		OPTDENSE::fNCCThresholdKeep * 1.5f : OPTDENSE::fNCCThresholdKeep);
+		OPTDENSE::fNCCThresholdKeep * 1.5f * 2.0f : OPTDENSE::fNCCThresholdKeep * 2.0f);
 	if (depthData.confMap.empty())
 		depthData.confMap.create(depthData.depthMap.size());
 	for (int r = 0; r < depthData.depthMap.rows; ++r) {
